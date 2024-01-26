2295dc6f5e28028f4d5ab0dd78c5140da7bc2b34
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-01-26 17:42:59 +0530

    ArrayList examples

--- advance_01_arraylist.java
+++ advance_01_arraylist.java
@@ -3,12 +3,17 @@ import java.util.*;
 public class advance_01_arraylist {
     public static void main(String[] args) {
         ArrayList<Integer> l1 = new ArrayList<>();
-
+        ArrayList<Integer> l2 = new ArrayList<>();
+        
         l1.add(3);
         l1.add(4);
         l1.add(5);
         l1.add(6);
         l1.add(7);
+        l2.add(36);
+        l2.add(44);
+        l1.addAll(l2);
+
         for (int i = 0; i < l1.size(); i++) {
             System.out.println(l1.get(i));
         }
