bee525bcbe086f23dd777854ce74080797421c3b
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-01-27 01:29:55 +0530

    Time in java

--- advance_06_calendar.java
+++ advance_06_calendar.java
@@ -1,4 +1,3 @@
-import java.sql.Time;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
---
+++ advance_07_time.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class advance_07_time {
+    public static void main(String[]args){
+        LocalDate d = LocalDate.now();
+        System.out.println(d);
+
+        LocalTime t = LocalTime.now();
+        System.out.println(t);
+        
+    }
+}
