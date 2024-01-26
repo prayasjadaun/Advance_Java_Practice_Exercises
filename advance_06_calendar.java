8bfd85e36fc62420dbe6ddcfe29a6a1c01c38d16
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-01-27 01:15:39 +0530

    TimeZone in java

--- advance_06_calendar.java
+++ advance_06_calendar.java
@@ -1,10 +1,19 @@
+import java.sql.Time;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
 public class advance_06_calendar {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Calendar c = Calendar.getInstance();
         System.out.println(c.getCalendarType());
         System.out.println(c.getTimeZone().getID());
+
+        GregorianCalendar cal = new GregorianCalendar();
+
+        System.out.println(cal.isLeapYear(2020));
+        for (int i = 0; i < 10; i++) {
+            System.out.println(TimeZone.getAvailableIDs()[i]);
+        }
     }
 }
