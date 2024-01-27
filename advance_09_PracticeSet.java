ccddde2202e888feb986a271d21642501faa0cdc
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-01-27 17:41:34 +0530

    Practice set advance java

--- advance_09_PracticeSet.java
+++ advance_09_PracticeSet.java
@@ -30,7 +30,7 @@ public class advance_09_PracticeSet {
 
         //Question--3
         Calendar c = Calendar.getInstance();
-        System.out.println(c.get(Calendar.HOUR)+":"+c.get(Calendar.MINUTE)+":"+c.get(Calendar.SECOND));
+        System.out.println(c.get(Calendar.HOUR_OF_DAY)+":"+c.get(Calendar.MINUTE)+":"+c.get(Calendar.SECOND));
 
         //Question--4
         LocalTime t = LocalTime.now();
